7d1b652483835cc5187cdedf494def6a87d3d027
Yasuhiro Яша Asaka <yasuhiro.asaka@grauwoelfchen.net>
2022-02-21 02:09:58 +0900

    Add WebView

--- src/bin/pumpfoil.rs
+++ src/bin/pumpfoil.rs
@@ -1,31 +1,46 @@
 extern crate gio;
 extern crate gtk;
+extern crate webkit2gtk;
 
 use std::env::args;
 
-use gio::prelude::*;
-use gtk::prelude::*;
-
+use gio::prelude::{ApplicationExt, ApplicationExtManual};
 use gtk::{AccelGroup, Application, ApplicationWindow, Builder};
+use gtk::prelude::{BuilderExtManual, ContainerExt, GtkWindowExt, WidgetExt};
+use webkit2gtk::{UserContentManager, WebContext, WebView, WebViewExtManual};
+use webkit2gtk::traits::{SettingsExt, WebViewExt};
+
+const DEFAULT_URI: &str = "https://duckduckgo.com";
 
 fn build_ui(app: &Application) {
-    let awin_src = include_str!("../ui/window.xml");
-    let awin: ApplicationWindow = Builder::from_string(awin_src)
-        .get_object("window")
-        .expect("couldn't get window");
-    awin.set_application(Some(app));
+    let win_src = include_str!("../ui/window.xml");
+    let win: ApplicationWindow = Builder::from_string(win_src)
+        .object("application_window")
+        .expect("couldn't get application window");
+    win.set_application(Some(app));
 
-    let accl_grp = AccelGroup::new();
-    awin.add_accel_group(&accl_grp);
-    awin.show_all();
+    let grp = AccelGroup::new();
+    win.add_accel_group(&grp);
+
+    // webview
+    let ctx = WebContext::default().unwrap();
+    let mgr = UserContentManager::new();
+    let web = WebView::new_with_context_and_user_content_manager(&ctx, &mgr);
+
+    let ext = WebViewExt::settings(&web).unwrap();
+    ext.set_enable_developer_extras(true);
+
+    web.load_uri(DEFAULT_URI);
+
+    win.add(&web);
+    win.show_all();
 }
 
 fn main() {
-    let application =
-        Application::new(Some("local.pumpfoil.desktop"), Default::default())
-            .expect("couldn't get initialized");
-    application.connect_activate(|app| {
-        build_ui(app);
+    let app =
+        Application::new(Some("local.pumpfoil.desktop"), Default::default());
+    app.connect_activate(|a| {
+        build_ui(a);
     });
-    application.run(&args().collect::<Vec<_>>());
+    app.run_with_args(&args().collect::<Vec<_>>());
 }
