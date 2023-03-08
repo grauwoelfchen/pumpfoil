d7d942d02c3d4fbf11abd388dcaecc8b89b07092
Yasuhiro Яша Asaka <yasuhiro.asaka@grauwoelfchen.net>
2023-03-08 22:30:50 +0900

    Update WebView settings and cookies

--- src/bin/pumpfoil.rs
+++ src/bin/pumpfoil.rs
@@ -7,10 +7,17 @@ use std::env::args;
 use gio::prelude::{ApplicationExt, ApplicationExtManual};
 use gtk::{AccelGroup, Application, ApplicationWindow, Builder};
 use gtk::prelude::{BuilderExtManual, ContainerExt, GtkWindowExt, WidgetExt};
-use webkit2gtk::{UserContentManager, WebContext, WebView, WebViewExtManual};
-use webkit2gtk::traits::{SettingsExt, WebViewExt};
+use webkit2gtk::{
+    CookieAcceptPolicy, CookiePersistentStorage, TLSErrorsPolicy,
+    UserContentManager, WebContext, WebView, WebViewExtManual,
+};
+use webkit2gtk::traits::{
+    CookieManagerExt, SettingsExt, WebContextExt, WebsiteDataManagerExt,
+    WebViewExt,
+};
 
 const DEFAULT_URI: &str = "https://duckduckgo.com";
+const COOKIE_FILE: &str = "./cookie.txt";
 
 fn build_ui(app: &Application) {
     let win_src = include_str!("../ui/window.xml");
@@ -24,11 +31,43 @@ fn build_ui(app: &Application) {
 
     // webview
     let ctx = WebContext::default().unwrap();
-    let mgr = UserContentManager::new();
-    let web = WebView::new_with_context_and_user_content_manager(&ctx, &mgr);
+    let mgr_u = UserContentManager::new();
+    let web = WebView::new_with_context_and_user_content_manager(&ctx, &mgr_u);
 
-    let ext = WebViewExt::settings(&web).unwrap();
-    ext.set_enable_developer_extras(true);
+    let stg = WebViewExt::settings(&web).unwrap();
+    stg.set_allow_file_access_from_file_urls(true);
+    stg.set_allow_universal_access_from_file_urls(true);
+    stg.set_enable_developer_extras(true);
+    stg.set_enable_javascript(true);
+    stg.set_enable_offline_web_application_cache(true);
+    stg.set_enable_page_cache(true);
+    stg.set_enable_smooth_scrolling(true);
+    stg.set_enable_write_console_messages_to_stdout(true);
+    stg.set_javascript_can_open_windows_automatically(true);
+    stg.set_javascript_can_access_clipboard(true);
+
+    // NOTE:
+    // Unfortunately, It seems that WebKit does not support handling secure
+    // cookies via HTTP on localhost, and also CSP is applied to it.
+    // So there is probably no way to enable it at the moment :'(
+    //
+    // Secure Cookie related:
+    //   * https://bugzilla.mozilla.org/show_bug.cgi?id=1618113
+    //   * https://github.com/tauri-apps/tauri/issues/2604
+    //   * https://github.com/tauri-apps/wry/issues/444
+    // CSP related:
+    //   * https://bugzilla.mozilla.org/show_bug.cgi?id=1447784
+    //   * https://bugs.webkit.org/show_bug.cgi?id=250776
+
+    let mgr_d = ctx.website_data_manager().unwrap();
+    // ITP (Intelligent Tracking Prevention) treats cookie policy as `Always``
+    mgr_d.set_itp_enabled(false);
+    mgr_d.set_tls_errors_policy(TLSErrorsPolicy::Ignore);
+
+    let mgr_c = ctx.cookie_manager().unwrap();
+    // for non-session cookies
+    mgr_c.set_persistent_storage(COOKIE_FILE, CookiePersistentStorage::Text);
+    mgr_c.set_accept_policy(CookieAcceptPolicy::NoThirdParty);
 
     web.load_uri(DEFAULT_URI);
 
